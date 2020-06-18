67300c769a98e2332781a16ed81e8baf6756464b
Victor Carvalho <victor.inf2011@hotmail.com>
2020-06-18 19:21:57 -0300

    finish of quick start

---
+++ cms/src/Model/Entity/Article.php
@@ -0,0 +1,13 @@
+<?php
+namespace App\Model\Entity;
+
+use Cake\ORM\Entity;
+
+class Article extends Entity
+{
+    protected $_accesible = [
+        '*' => true,
+        'id' => false,
+        'slug' => false,
+    ];
+}
